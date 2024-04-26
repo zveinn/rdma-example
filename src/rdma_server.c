f2570af531f1a9a281b0fa9e419cc0b6c5733e01
zveinn <info@sveinn.is>
2024-04-26 03:53:48 -0700

    update

--- src/rdma_server.c
+++ src/rdma_server.c
@@ -147,7 +147,6 @@ static int accept_client_connection(client *c) {
     return -errno;
   }
   debug("++ESTABLISHED(received!) \n");
-  debug("++EVENT(ID) %p \n", cm_event->id);
 
   ret = rdma_ack_cm_event(cm_event);
   if (ret) {
