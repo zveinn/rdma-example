d5ab217102fb8c7ac880b0dcfc1efc2a2cc85d3b
zveinn <info@sveinn.is>
2024-04-26 07:12:06 -0700

    update

--- src/rdma_client.c
+++ src/rdma_client.c
@@ -498,22 +498,22 @@ int main(int argc, char **argv) {
     rdma_error("Failed to setup client connection , ret = %d \n", ret);
     return ret;
   }
-  // ret = client_pre_post_recv_buffer();
-  // if (ret) {
-  //   rdma_error("Failed to setup client connection , ret = %d \n", ret);
-  //   return ret;
-  // }
+  ret = client_pre_post_recv_buffer();
+  if (ret) {
+    rdma_error("Failed to setup client connection , ret = %d \n", ret);
+    return ret;
+  }
   ret = client_connect_to_server();
   if (ret) {
     rdma_error("Failed to setup client connection , ret = %d \n", ret);
     return ret;
   }
 
-  ret = client_xchange_metadata_with_server();
-  if (ret) {
-    rdma_error("Failed to setup client connection , ret = %d \n", ret);
-    return ret;
-  }
+  // ret = client_xchange_metadata_with_server();
+  // if (ret) {
+  //   rdma_error("Failed to setup client connection , ret = %d \n", ret);
+  //   return ret;
+  // }
 
   // ret = client_remote_memory_ops();
   // if (ret) {
