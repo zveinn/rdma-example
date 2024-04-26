c99f80df52c39d24a6381605349735d7795207bc
zveinn <info@sveinn.is>
2024-04-26 03:20:41 -0700

    update

--- src/rdma_server.c
+++ src/rdma_server.c
@@ -88,7 +88,7 @@ static int setup_client_resources(client *c) {
   c->QP.cap.max_recv_wr = MAX_WR;
   c->QP.cap.max_send_sge = MAX_SGE;
   c->QP.cap.max_send_wr = MAX_WR;
-  c->QP.qp_type = IBV_QPT_UC;
+  c->QP.qp_type = IBV_QPT_UD;
   c->QP.recv_cq = c->CQ;
   c->QP.send_cq = c->CQ;
   ret = rdma_create_qp(c->cm_event_id, c->PD, &c->QP);
