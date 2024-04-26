ca03d5b18f7e5dbc2d6eac820a57aed01b96ad4e
zveinn <info@sveinn.is>
2024-04-26 04:54:41 -0700

    update

--- src/rdma_server.c
+++ src/rdma_server.c
@@ -392,7 +392,7 @@ static void acceptConnection(struct rdma_cm_event *event) {
   int i;
   for (i = 0; i < 10000; i++) {
     if (requested_clients[i] != 0) {
-      printf("COMPARE: %d == %d",
+      printf("COMPARE: %d == %d\n",
              requested_clients[i]->cm_event->param.conn.qp_num,
              event->param.conn.qp_num);
 
