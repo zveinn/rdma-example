4945e2c67f405884cd5a5f0538d86f848886a351
zveinn <info@sveinn.is>
2024-04-29 07:57:35 -0700

    update

--- gotest/main.go
+++ gotest/main.go
@@ -3,7 +3,11 @@ package main
 // #cgo LDFLAGS: -lrdmacm
 // #include <rdma/rdma_cma.h>
 import "C"
-import "fmt"
+
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	RdmaCreateEventChannel()
@@ -12,4 +16,7 @@ func main() {
 func RdmaCreateEventChannel() {
 	channel := C.rdma_create_event_channel()
 	fmt.Println("CHAN:", channel)
+	fmt.Printf("CHAN: %p", channel)
+	fmt.Printf("CHAN: %p", &channel)
+	fmt.Printf("CHANU: %p", unsafe.Pointer(uint32(channel)))
 }
