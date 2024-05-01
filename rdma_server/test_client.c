6df9fab8fcc7eb3f717e06451e4f424d22eae6e0
zveinn <info@sveinn.is>
2024-05-01 09:59:34 -0700

    update

--- rdma_server/test_client.c
+++ rdma_server/test_client.c
@@ -46,7 +46,7 @@ static int client_prepare_connection(struct sockaddr_in *s_addr) {
   /* rdma_cm_id is the connection identifier (like socket) which is used
    * to define an RDMA connection.
    */
-  ret = rdma_create_id(cm_event_channel, &cm_client_id, NULL, RDMA_PS_UDP);
+  ret = rdma_create_id(cm_event_channel, &cm_client_id, NULL, RDMA_PS_TCP);
   if (ret) {
     debug("Creating cm id failed with errno: %d \n", -errno);
     return -errno;
