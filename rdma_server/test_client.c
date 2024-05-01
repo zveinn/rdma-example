2919cf5ca334a99bba61382911def0cbb7ef7dd4
zveinn <info@sveinn.is>
2024-05-01 09:54:34 -0700

    update

--- rdma_server/test_client.c
+++ rdma_server/test_client.c
@@ -268,12 +268,12 @@ static int client_xchange_metadata_with_server() {
   bzero(&client_send_wr, sizeof(client_send_wr));
   client_send_wr.sg_list = &client_send_sge;
   client_send_wr.num_sge = 1;
-  client_send_wr.opcode = IBV_WR_SEND;
+  client_send_wr.opcode = IBV_WR_SEND_WITH_IMM;
   client_send_wr.send_flags = IBV_SEND_SIGNALED;
   printf("D4 \n");
-  printf("1:%p", client_qp);
-  printf("2:%p", &client_send_wr);
-  printf("3:%p", &bad_client_send_wr);
+  printf("1:%p\n", client_qp);
+  printf("2:%p\n", &client_send_wr);
+  printf("3:%p\n", &bad_client_send_wr);
   printf("D4 \n");
   /* Now we post it */
   ret = ibv_post_send(client_qp, &client_send_wr, &bad_client_send_wr);
